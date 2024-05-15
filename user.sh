62e91857bcee1d2c25f92d320f22921eb5b38228
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-05-15 15:09:11 -0700

    Added new sed command for changing password auth

--- user.sh
+++ user.sh
@@ -5,5 +5,6 @@ sudo timedatectl set-timezone America/Los_Angeles
 sudo echo "terra ALL=(ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/terra
 sudo su - terra
 sudo chown terra:terra -R /etc/ssh/sshd_config
-sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
+sudo sed -i 's/no/yes/' /etc/ssh/sshd_config.d/*.conf
+#sudo sed -i "/^[^#]*PasswordAuthentication[[:space:]]no/c\PasswordAuthentication yes" /etc/ssh/sshd_config
 sudo service sshd restart
