d4481b1ecb337059c0d61a958838e24cb0dbe54e
Babs <68171102+Ohubabs@users.noreply.github.com>
2024-05-16 11:31:42 -0700

    Added module to install cert-manager add-on service permissions

--- eks/svc.tf
+++ eks/svc.tf
@@ -75,3 +75,22 @@ module "alb_role" {
 
 #module.load_balancer_controller_irsa_role.aws_iam_policy.load_balancer_controller.id create an output for this
 #module.load_balancer_controller_irsa_role.aws_iam_policy.id
+
+module "cert_manager_role" {
+  source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+
+  role_name                     = "DB-cert-manager"
+  attach_cert_manager_policy    = true
+  cert_manager_hosted_zone_arns = ["arn:aws:route53:::hostedzone/<hosted-zone-id>"]
+
+  oidc_providers = {
+    ex = {
+      provider_arn               = module.eks.oidc_provider_arn
+      namespace_service_accounts = ["cert-manager:cert-manager"]
+    }
+  }
+
+  tags = {
+    name = "kubernetes.io/cluster/DB"
+  }
+}
